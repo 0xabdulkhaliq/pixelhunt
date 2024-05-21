43bee07c99ed07f5231b21f98dba40a6be16eda2
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-21 16:00:39 +0530

    feat: implement header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
-export default function Header() {}
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-10 bg-white p-2 md:p-3 border-b border-solid border-b-gray-300 mb-2 shadow-[0_30px_20px_#f9fafb]">
+      <p className="font-medium text-2xl">
+        Pixel<span className="text-primary">Hunt</span>
+      </p>
+    </header>
+  );
+}
