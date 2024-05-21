3e079c814e207567413a0e1e228103686712b8a0
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-21 15:00:41 +0530

    feat: implement google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        primary: "#4f46e5",
+      },
+    },
   },
   plugins: [],
 };
