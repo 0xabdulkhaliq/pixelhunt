1f5c3150518acd73b2989968a0607338ebe84e61
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-23 15:50:58 +0530

    feat: create tooltip for marker

--- src/pages/Gameboard.jsx
+++ src/pages/Gameboard.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Gameboard() {
   const [coords, setCoords] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState(true);
+  const markerRef = useRef(null);
 
   function getCoords(e) {
     const imageRect = e.target.getBoundingClientRect();
@@ -12,18 +14,44 @@ export default function Gameboard() {
     setCoords({ X: x, Y: y });
   }
 
+  useEffect(() => {
+    if (coords) {
+      const marker = markerRef.current.getBoundingClientRect();
+      const screenWidth = window.innerWidth;
+      const rightSpace = screenWidth - marker.right;
+
+      setTooltipPosition(rightSpace >= 110);
+    }
+  }, [coords]);
+
   return (
     <main>
       <div className="relative">
         <img src="words-waldo.webp" alt="Waldo" onClick={getCoords} />
         {coords && (
           <div
+            ref={markerRef}
             style={{
               translate: `${coords.X - 28}px ${coords.Y - 28}px`,
             }}
             className="grid absolute top-0 left-0 place-items-center -outline-offset-[.3rem] w-14 h-14 rounded-full outline-dashed outline-3 outline-white bg-[#0008]"
           >
             <div className="w-2 h-2 bg-white rounded-full"></div>
+
+            <ul
+              style={{
+                translate: `${tooltipPosition ? "6" : "-6"}rem`,
+              }}
+              className="absolute min-w-24 top-0 overflow-hidden outline outline-1 outline-gray-400 shadow-2xl rounded-md"
+            >
+              {[1, 2, 3].map((el, index) => (
+                <li key={index} className="w-full">
+                  <button className="px-5 w-full py-3 outline font-normal outline-1 outline-gray-400 text-start bg-gray-50 transition-[background] md:hover:bg-gray-200 active:bg-gray-300">
+                    Target {el}
+                  </button>
+                </li>
+              ))}
+            </ul>
           </div>
         )}
       </div>
