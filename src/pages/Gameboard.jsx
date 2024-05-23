2ab7b2a04a75676d585703a8de468eeead1061fd
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-23 14:46:31 +0530

    feat: initialize waldo gameboard

--- src/pages/Gameboard.jsx
+++ src/pages/Gameboard.jsx
@@ -1,1 +1,32 @@
-export default function Gameboard() {}
+import { useState } from "react";
+
+export default function Gameboard() {
+  const [coords, setCoords] = useState(false);
+
+  function getCoords(e) {
+    const imageRect = e.target.getBoundingClientRect();
+    const x = e.clientX - imageRect.left;
+    const y = e.clientY - imageRect.top;
+
+    console.log(`X: ${x}px, Y: ${y}px`);
+    setCoords({ X: x, Y: y });
+  }
+
+  return (
+    <main>
+      <div className="relative">
+        <img src="words-waldo.webp" alt="Waldo" onClick={getCoords} />
+        {coords && (
+          <div
+            style={{
+              translate: `${coords.X - 28}px ${coords.Y - 28}px`,
+            }}
+            className="grid absolute top-0 left-0 place-items-center -outline-offset-[.3rem] w-14 h-14 rounded-full outline-dashed outline-3 outline-white bg-[#0008]"
+          >
+            <div className="w-2 h-2 bg-white rounded-full"></div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,11 @@
-export default function Home() {}
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <main className="mx-auto">
+      <Link to={"/gameboard"} className="btn-primary w-max">
+        Game
+      </Link>
+    </main>
+  );
+}
