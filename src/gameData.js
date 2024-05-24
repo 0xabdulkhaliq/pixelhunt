9593b4e7a755bbaafb5e92cfad54d8dc38f8ec77
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-24 20:33:25 +0530

    feat: create game data

---
+++ src/gameData.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    title: "Waldo's Fusion",
+    image: "https://i.ibb.co/WKn2kdh/waldo-1.webp",
+    targets: [
+      {
+        name: "Puppet Monkey",
+        image: "https://i.ibb.co/chMPxyP/puppet-monkey.webp",
+        marker: { x: 1581, y: 1264 },
+      },
+      {
+        name: "Waldo",
+        image: "https://i.ibb.co/phXpK4R/waldo.webp",
+        marker: { x: 429, y: 1184 },
+      },
+      {
+        name: "Courage",
+        image: "https://i.ibb.co/9ZBL6Mq/courage.webp",
+        marker: { x: 907, y: 600 },
+      },
+    ],
+  },
+  {
+    title: "Super Mario's Island",
+    image: "https://i.ibb.co/3pfc5TV/waldo-2.webp",
+    targets: [
+      {
+        name: "Green Paratroopa",
+        image: "https://i.ibb.co/vsHVgRm/green-paratroopa.webp",
+        marker: { x: 1729, y: 719 },
+      },
+      {
+        name: "Princess Peach",
+        image: "https://i.ibb.co/wsndn14/peach.webp",
+        marker: { x: 599, y: 921 },
+      },
+      {
+        name: "Toad",
+        image: "https://i.ibb.co/Sr5Q6fm/toad.webp",
+        marker: { x: 407, y: 655 },
+      },
+    ],
+  },
+  {
+    title: "Gamers Waldo",
+    image: "https://i.ibb.co/kBswwyN/waldo-3.webp",
+    targets: [
+      {
+        name: "Pikachu",
+        image: "https://i.ibb.co/0CJphMF/pikachu.webp",
+        marker: { x: 573, y: 690 },
+      },
+      {
+        name: "Knuckles",
+        image: "https://i.ibb.co/Nt4Ywhr/knuckles.webp",
+        marker: { x: 1434, y: 974 },
+      },
+      {
+        name: "Pea Shooter",
+        image: "https://i.ibb.co/P1ryFx3/peashooter.webp",
+        marker: { x: 397, y: 1137 },
+      },
+    ],
+  },
+];
