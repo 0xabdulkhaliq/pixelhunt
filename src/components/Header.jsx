2af58f1c8c500dc597c560a590b6a6aae7f2dd2f
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-25 19:17:38 +0530

    refactor: implement link to homepage on header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 bg-white p-2 md:p-3 border-b border-solid border-b-gray-300 mb-2 shadow-[0_30px_20px_#f9fafb]">
-      <p className="font-medium text-2xl">
+    <header className="sticky top-0 transition-shadow duration-200 z-10 bg-white p-2 md:p-3 border-b border-solid border-b-gray-300">
+      <Link to={"/"} className="font-medium text-2xl">
         Pixel<span className="text-primary">Hunt</span>
-      </p>
+      </Link>
     </header>
   );
 }
